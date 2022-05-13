57f7e832be077753337ecf5f92f5809f08470cdc
J-D25 <jd.auzanneau@icloud.com>
2022-05-13 10:01:42 +0200

    index.php : Adding alt

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
         <div id="home_section_1">
             <h2>La meilleure basilique du monde</h2>
             <div id="home_section_1_content">
-                <img src="images/main_01.png" width="372" height="290">
+                <img src="images/main_01.png" width="372" height="290" alt="Vue aérienne de la basilique Saint-Ferjeux en noir et blanc du vingtième siècle.">
                 <div>
                     <p>Vivamus sit amet pretium odio. Vestibulum nec hendrerit ligula, id lacinia libero. Nullam vulputate viverra sem nec consectetur. In pharetra laoreet leo et iaculis. Suspendisse aliquet eleifend erat, quis auctor velit aliquam eget.
                         Vivamus fringilla sapien pretium, fringilla erat at, venenatis mauris. Mauris pulvinar nisi a leo consequat pellentesque. Sed sem dolor, convallis quis enim ut, tincidunt dictum arcu. Mauris ultrices nec ligula commodo aliquet.
@@ -32,27 +32,27 @@
             </div>
         </div>
         <div id="home_section_2">
-            <img class="home_panorama" src="images/main_02.png" width="1440" height="536">
+            <img class="home_panorama" src="images/main_02.png" width="1440" height="536" alt="Basilique Saint-Ferjeux vue de côté du style romano-byzantin. Elle est composée de deux immenses tours situé à l'avant, suivie de la grande salle rectangulaire surplombée d'une coupole. Fini ensuite par une abside. On peut voir au premier plan le cimetière de Saint-Ferjeux.">
         </div>
         <div id="home_section_3">
             <h3 class="bottomline">Agenda</h3>
             <div id="home_section_3_content">
                 <div class="home_section_3_content_actu">
-                    <img src="images/actu_01.png" width="340" height="226">
+                    <img src="images/actu_01.png" width="340" height="226" alt="Quatre personnes debout prient dans un lieu de culte chrétien. Un prêtre les regarde. Une lumière blanche les illumine par un vitrail en forme de rosace.">
                     <div class="home_section_3_content_actu_text">
                         <h4>Concert du 26 juin 2022</h4>
                         <p>L’Association Musique Sacrée, sa maîtrise et ses solistes, proposent des pièces musicales de Mozart, Bach, Mendelson, Monteverdi…</p>
                     </div>
                 </div>
                 <div class="home_section_3_content_actu">
-                    <img src="images/actu_02.png" width="340" height="226">
+                    <img src="images/actu_02.png" width="340" height="226" alt="Gros plan sur les mains d'une personne de foi. Elle prie. Ses doigts sont croisés.">
                     <div class="home_section_3_content_actu_text">
                         <h4>Messe de l’Ascension</h4>
                         <p>La messe de l’Ascension se tiendra le jeudi 26 mai à partir de 10 h 30. L'archevêque de Besançon, Monseigneur Jean-Luc Bouilleret, présidera la messe.</p>
                     </div>
                 </div>
                 <div class="home_section_3_content_actu">
-                    <img src="images/actu_03.png" width="340" height="226">
+                    <img src="images/actu_03.png" width="340" height="226" alt="Cinq personnes sont dans un lieu de culte chrétien. Il y a deux hommes et une femme assis sur des bancs. La femme lit la bible. Un prêtre montre un texte de la bible à une autre femme.">
                     <div class="home_section_3_content_actu_text">
                         <h4>1<sup>er</sup> mai, fête du travail</h4>
                         <p>Instituée initialement en mémoire de la grève générale du 1<sup>er</sup> mai 1886 à Chicago, la fête du travail a été adoptée progressivement au cours du XX<sup>e</sup> siècle.</p>
@@ -61,8 +61,8 @@
             </div>
         </div>
         <div id="home_section_4">
-            <div id="home_section_4_1"><img src="images/main_03.png" width="402" height="446"></div>
-            <div id="home_section_4_2"><img src="images/main_04.png" width="650" height="446"></div>
+            <div id="home_section_4_1"><img src="images/main_03.png" width="402" height="446" alt="Photo de la basilique vue de face. On voit les deux immenses tours, ainsi que sa façade antérieure. Il y a cinq statues d'apôtres incrustées dans celle-ci."></div>
+            <div id="home_section_4_2"><img src="images/main_04.png" width="650" height="446" alt="Photo de la basilique en noir et blanc datant du vingtième siècle."></div>
         </div>
     </main>
     <footer>
