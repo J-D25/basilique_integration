58d13f7fee11ad481fa69462f34664be3da93588
J-D25 <jd.auzanneau@icloud.com>
2022-06-07 15:15:37 +0200

    script.js : adding form verif

--- script.js
+++ script.js
@@ -8,3 +8,21 @@ checkbox.addEventListener('click', event => {
         hook.style.display = "flex";
     }
 })
+
+const form = document.querySelector('form');
+const btnSubmit = document.querySelector('#contact_form_button');
+
+form.addEventListener('submit', function(e) {
+    if (!form.checkValidity()) {
+        e.preventDefault()
+    }
+    const inputs = document.querySelectorAll('.contact_form_input');
+
+    inputs.forEach((input) => {
+        if (!input.checkValidity()) {
+            input.classList.add("contact_form_invalid");
+        } else {
+            input.classList.remove("contact_form_invalid");
+        }
+    })
+})
