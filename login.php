877cb56f29e3a0d85bb057a9602e3984e8dc3e20
J-D25 <jd.auzanneau@icloud.com>
2022-06-15 16:58:20 +0200

    login.php : create login page

---
+++ login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Auteur:J-D25, Projet ACS 2022">
+    <link rel="stylesheet" href="style2.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Akshar:wght@300;400;500&family=Glory&display=swap" rel="stylesheet">
+    <title>Basilique Saint-Ferjeux - Connexion</title>
+</head>
+<body>
+    <main>
+        <form>
+            <label for="username">Nom d'utilisateur</label>
+            <input type="text" name="username">
+            <label for="username">Mot de passe</label>
+            <input type="password" name="password">
+            <input type="button" value="Valider">
+        </form>
+    </main>
+</body>
+</html>
