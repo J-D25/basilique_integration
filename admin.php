d88383f9abace1a9be3cf6e323f86d7afa80baca
J-D25 <jd.auzanneau@icloud.com>
2022-06-28 15:15:50 +0200

    admin.php : fix html syntax

--- admin.php
+++ admin.php
@@ -15,12 +15,12 @@
 </head>
 
 <body>
+    <header>
+        <input type="text" placeholder="Rechercher" id="search">
+        <input type="button" value="Déconnexion" id="logout">
+    </header>
+
     <main>
-        <header>
-            <input type="text" placeholder="Rechercher" id="search">
-            <input type="button" value="Déconnexion" id="logout">
-        </header>
-        
         <table>
             <thead>
                 <tr class="head">
@@ -30,22 +30,23 @@
                 </tr>
             </thead>
             <tbody>
-                
+
             </tbody>
         </table>
-        <footer>
-            <input type="button" value="Charger plus" class="button_blue">
-            <input type="button" value="Exporter en csv" class="button_blue" id="export">
-        </footer>
     </main>
-    
-<template id="templatemail">
-  <tr>
-    <td class="admin_email"></td>
-    <td class="admin_date"></td>
-    <td class="admin_bin"><svg width="12" height="14" viewBox="0 0 12 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M8.64458 1.16667H11.5261V2.33333H0V1.16667H2.88153L3.84633 0H7.67981L8.64458 1.16667ZM2.68944 14C1.8441 14 1.15261 13.3017 1.15261 12.4479V3.5H10.3735V12.4479C10.3735 13.3017 9.682 14 8.8367 14H2.68944Z" fill="#ED6368"/></svg></td>
-  </tr>
-</template>
+
+    <footer>
+        <input type="button" value="Charger plus" class="button_blue">
+        <input type="button" value="Exporter en csv" class="button_blue" id="export">
+    </footer>
+
+    <template id="templatemail">
+        <tr>
+            <td class="admin_email"></td>
+            <td class="admin_date"></td>
+            <td class="admin_bin"><svg width="12" height="14" viewBox="0 0 12 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M8.64458 1.16667H11.5261V2.33333H0V1.16667H2.88153L3.84633 0H7.67981L8.64458 1.16667ZM2.68944 14C1.8441 14 1.15261 13.3017 1.15261 12.4479V3.5H10.3735V12.4479C10.3735 13.3017 9.682 14 8.8367 14H2.68944Z" fill="#ED6368"/></svg></td>
+        </tr>
+    </template>
 
 </body>
 
