4e579b54badb528bb6e05b967f0dcd9a3688a7a1
J-D25 <jd.auzanneau@icloud.com>
2022-06-28 14:53:58 +0200

    search.php : adding script for searchbar

---
+++ search.php
@@ -0,0 +1,30 @@
+<?php
+require __DIR__ .'/vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+define('SERVER', $_ENV['SERVER']);
+define('DATABASE', $_ENV['DATABASE']);
+define('USERNAME', $_ENV['USERNAME']);
+define('PASSWORD', $_ENV['PASSWORD']);
+
+$_POST = json_decode(file_get_contents('php://input'), true);
+
+if (isset($_POST) && !empty($_POST)) {
+    $errorCode = true;
+    try{
+        $conn = new PDO("mysql:host=".SERVER.";dbname=".DATABASE."", USERNAME, PASSWORD);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sth = $conn->prepare("SELECT `email`, `date` FROM `newsletter` WHERE `email` LIKE (:email) ORDER BY `email`");
+        $sth->bindValue(':email', '%'.$_POST.'%', PDO::PARAM_STR);
+        $sth->execute();
+        $res = $sth->fetchAll(PDO::FETCH_ASSOC);
+    }
+    catch(PDOException $e){
+        $errorCode = $e->getCode();
+    }
+    $conn = null;
+    echo json_encode(["responseServer"=>true, "responseDB"=>$errorCode, "data"=>$res]);
+} else {
+    echo json_encode(["responseServer"=>false]);
+}
+?>
