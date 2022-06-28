6f3ea953bca387e20a2b154665eb85784e6785c9
J-D25 <jd.auzanneau@icloud.com>
2022-06-28 14:05:31 +0200

    script2.js : adding AJAX query for mail listing

--- script2.js
+++ script2.js
@@ -1,21 +1,45 @@
-const bins = document.querySelectorAll(".admin_bin");
-const emails = Array.from(document.querySelectorAll(".admin_email"));
+window.addEventListener("DOMContentLoaded", () => {
+    listingMail();
+})
 
-bins.forEach((bin, binIndex) => {
-    bin.addEventListener("click", function() {
-        const data = emails[binIndex].textContent;
+function listingMail() {
+    // const tBody = document.querySelector("TBODY");
+    // tBody.remove();
+    const template = document.querySelector("#templatemail");
 
-        fetch("delete.php", {
-                method: "POST",
-                body: JSON.stringify(data),
-                contentType: 'application/json'
-            })
-            .then(res => res.text())
-            .then((results) => {
-                let resultsJSON = JSON.parse(results)
-                if (resultsJSON.responseServer === true && resultsJSON.responseDB === true) {
-                    alert("Suppression effectuée");
-                }
+    fetch("list.php")
+        .then(res => res.text())
+        .then((results) => {
+            const resultsJSON = JSON.parse(results);
+            const tableNewsletter = resultsJSON['data'];
+            let i = 0;
+            tableNewsletter.forEach((element, index) => {
+                const tbody = document.querySelector("tbody");
+                const clone = document.importNode(template.content, true);
+                const tr = clone.querySelectorAll("tr");
+                const td = clone.querySelectorAll("td");
+                td[0].textContent = tableNewsletter[index]['email'];
+                td[1].textContent = tableNewsletter[index]['date'];
+                td[2].addEventListener("click", function() {
+                    const data = td[0].textContent;
+                    const dialog = confirm("Voulez-vous supprimer " + data + " ?");
+                    if (dialog) {
+                        fetch("delete.php", {
+                                method: "POST",
+                                body: JSON.stringify(data),
+                                contentType: 'application/json'
+                            })
+                            .then(res => res.text())
+                            .then((results) => {
+                                let resultsJSON = JSON.parse(results)
+                                if (resultsJSON.responseServer === true && resultsJSON.responseDB === true) {
+                                    alert("Suppression effectuée.");
+                                    tr[0].remove();
+                                }
+                            });
+                    }
+                });
+                tbody.appendChild(clone);
             });
-    });
-});
+        });
+}
