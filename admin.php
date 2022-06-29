2587b7627fa800021d5ae4da3549f6e88786ee6e
J-D25 <jd.auzanneau@icloud.com>
2022-06-29 16:38:15 +0200

    changing href of stylesheets

--- admin.php
+++ admin.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Auteur:J-D25, Projet ACS 2022">
-    <link rel="stylesheet" href="style3.css">
+    <link rel="stylesheet" href="assets/css/style3.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Akshar:wght@300;400;500&family=Glory:wght@400&display=swap" rel="stylesheet">
