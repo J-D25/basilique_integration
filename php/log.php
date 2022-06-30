7a85cdf2c618b449900058d5c623804fcabb1b46
J-D25 <jd.auzanneau@icloud.com>
2022-06-30 19:58:08 +0200

    php/log.php : using PASSWORD_BCRYPT for password

--- php/log.php
+++ php/log.php
@@ -6,9 +6,8 @@ if (isset($_POST['username']) && !empty($_POST['username']) && isset($_POST['pas
     try{
         $conn = new PDO("mysql:host=".SERVER.";dbname=".DATABASE."", USERNAME, PASSWORD);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $sth = $conn->prepare("SELECT `username`, `password` FROM `users` WHERE `username`=(:username) AND `password`=(:password)");
+        $sth = $conn->prepare("SELECT `username`, `password` FROM `users` WHERE `username`=(:username)");
         $sth->bindValue(':username', $_POST['username'], PDO::PARAM_STR);
-        $sth->bindValue(':password', MD5($_POST['password']), PDO::PARAM_STR);
         $sth->execute();
         $res = $sth->fetch(PDO::FETCH_ASSOC);
     }
@@ -16,7 +15,11 @@ if (isset($_POST['username']) && !empty($_POST['username']) && isset($_POST['pas
         $errorCode = $e->getCode();
     }
     $conn = null;
-    echo json_encode(["responseServer"=>true, "responseDB"=>$errorCode, "data"=>$res]);
+    if (password_verify($_POST['password'], $res['password'])){
+        echo json_encode(["responseServer"=>true, "responseDB"=>$errorCode, "connection"=>true]);
+    } else {
+        echo json_encode(["responseServer"=>true, "responseDB"=>$errorCode, "connection"=>false]);
+    }
 } else {
     echo json_encode(["responseServer"=>false]);
 }
