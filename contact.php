0f9cc4dd05bec98b04cac7c47a82fceb33a13f36
J-D25 <jd.auzanneau@icloud.com>
2022-07-05 17:31:37 +0200

    contact.php : remove meta IE=Edge, add meta author, add meta description, remove onsubmit

--- contact.php
+++ contact.php
@@ -3,9 +3,9 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Auteur:J-D25, Projet ACS 2022">
+    <meta name="author" content="J-D25">
+    <meta name="description" content="Vous avez une question concernant la Basilique Saint-Ferjeux ? N'hésitez pas à nous contacter.">
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -13,7 +13,7 @@
     <script src="js/main.js" defer></script>
     <script src="js/contactform.js" defer></script>
     <noscript><style>img {transform: translateY(0px); opacity: 1;}</style></noscript>
-    <title>Basilique Saint-Ferjeux</title>
+    <title>Basilique Saint-Ferjeux - Contact</title>
 </head>
 
 <body>
@@ -23,7 +23,7 @@
     <main>
         <h2>Contactez-nous</h2>
         <p>Vous avez une question concernant notre basilique ? Vous pouvez nous joindre à tout moment via le formulaire de contact ci-dessous. N'hésitez pas à nous contacter, nous serons très heureux de vous répondre !<br>Tous les champs de ce formulaire sont obligatoires.</p>
-        <form id="contact_form" method="post" novalidate onsubmit="return ajaxpost()">
+        <form id="contact_form" method="post" novalidate>
             <div id="contact_form_flexname">
                 <div id="contact_form_flexname_name">
                     <label for="contact_form_name">Nom</label>
@@ -37,7 +37,7 @@
             <label for="contact_form_email">Email</label>
             <input type="email" name="mail" id="contact_form_email" class="contact_form_input" aria-required="true" required/>
             <label for="contact_form_object">Objet</label>
-            <input type="text"  name="objet" id="contact_form_object" class="contact_form_input" aria-required="true" required/>
+            <input type="text" name="objet" id="contact_form_object" class="contact_form_input" aria-required="true" required/>
             <label for="contact_form_message">Message</label>
             <textarea name="message" id="contact_form_message" rows="8" class="contact_form_input" aria-required="true" required></textarea>
             <div class="button">
