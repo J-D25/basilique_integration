fceb5d571e2a0d25e177f8d19ee5303be09b93e9
J-D25 <jd.auzanneau@icloud.com>
2022-07-05 21:46:20 +0200

    admin.php : add #header_title, remove meta IE=Edge

--- admin.php
+++ admin.php
@@ -12,7 +12,6 @@ if($_SESSION['user']!="admin"){
 
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Auteur:J-D25, Projet ACS 2022">
     <link rel="stylesheet" href="assets/css/style3.css">
@@ -23,7 +22,9 @@ if($_SESSION['user']!="admin"){
     <title>Basilique Saint-Ferjeux - Administration</title>
 </head>
 
-<body><header>
+<body>
+    <header>
+        <p id="header_title">Basilique Saint-Ferjeux : Dashboard</p>
         <a href="php/logout.php" title="Déconnexion" id="logout">
             <svg xmlns:svg="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 891 980"><g transform="translate(-54.5,-10)"><path d="m 143.6,544.5 c 0,-131.9 71.7,-247.1 178.2,-308.7 V 136 C 164.5,204.8 54.5,361.9 54.5,544.5 54.5,790.6 254,990 500,990 746,990 945.5,790.6 945.5,544.5 945.5,361.8 835.5,204.7 678.2,136 v 99.8 C 784.7,297.4 856.4,412.6 856.4,544.5 856.4,741.3 696.9,900.9 500,900.9 303.1,900.9 143.6,741.4 143.6,544.5 Z M 500,10 c -49.2,0 -89.1,39.9 -89.1,89.1 v 356.4 c 0,49.2 39.9,89.1 89.1,89.1 49.2,0 89.1,-39.9 89.1,-89.1 V 99.1 C 589.1,49.9 549.2,10 500,10 Z" style="fill:#ffffff"/></g></svg>
         </a>
