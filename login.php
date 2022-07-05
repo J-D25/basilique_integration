b7882d34a9332ca78e553ae79957eb652b70a433
J-D25 <jd.auzanneau@icloud.com>
2022-07-05 21:46:37 +0200

    login.php : remove meta IE=Edge

--- login.php
+++ login.php
@@ -3,7 +3,6 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Auteur:J-D25, Projet ACS 2022">
     <link rel="stylesheet" href="assets/css/style2.css">
