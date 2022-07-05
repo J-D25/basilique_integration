b15d04a511435a072b583b60cfed688af5cf93d7
J-D25 <jd.auzanneau@icloud.com>
2022-07-05 10:04:36 +0200

    js/login.js : Add error span

--- js/login.js
+++ js/login.js
@@ -1,8 +1,39 @@
 const formLogin = document.querySelector("#login_form");
+const inputLogin = document.querySelectorAll('.login_input');
+const btnSubmitLogin = document.querySelector('#login_button');
 
 formLogin.addEventListener('submit', function(e) {
     e.preventDefault();
+    checkLogin();
+    ajaxLogin();
+});
 
+function checkLogin() {
+    inputLogin.forEach((input) => {
+        const input_span = document.createElement("SPAN");
+        const cannot = "Ce champ ne peut être vide";
+        let input_span_text = document.createTextNode(cannot);
+        input_span.appendChild(input_span_text);
+        input_spanInvalid = document.getElementById(input.id + "_invalid");
+        input_span.setAttribute("class", "login_form_span_invalid");
+        input_span.setAttribute("id", input.id + "_invalid");
+        if (input.validity.valueMissing) {
+            input.classList.add("login_form_invalid");
+            if (!input_spanInvalid) {
+                input.insertAdjacentElement('afterend', input_span);
+            } else {
+                input_spanInvalid.replaceWith(input_span);
+            }
+        } else {
+            input.classList.remove("login_form_invalid");
+            if (input_spanInvalid) {
+                input_spanInvalid.remove();
+            }
+        }
+    })
+}
+
+function ajaxLogin() {
     const formData = new FormData(formLogin);
     fetch("php/log.php", {
             method: "POST",
@@ -15,10 +46,6 @@ formLogin.addEventListener('submit', function(e) {
                 document.location.href = "admin.php";
             } else if (resultsJSON.responseServer === true && resultsJSON.responseDB === true && resultsJSON.connection === false) {
                 alert("Identifiant ou mot de passe incorrect.");
-            } else if (resultsJSON.responseServer === false) {
-                alert("Merci de remplir tous les champs.");
-            } else {
-                alert("Une erreur s'est produite.");
             }
         })
-});
+}
