1b6d5f6a8a3e2198dcc422f3f1c98e6e40aedac0
J-D25 <jd.auzanneau@icloud.com>
2022-07-05 20:30:44 +0200

    index.php, en_savoir_plus.php : add .imgcontainer, change <div> to <p>

--- en_savoir_plus.php
+++ en_savoir_plus.php
@@ -33,37 +33,26 @@
     <main id="esp">
         <div id="esp_section_1">
             <h2>Historique de la basilique</h2>
-            <div id="esp_section_1_gallery" class="panorama"></div>
+            <p id="esp_section_1_gallery" class="panorama img_container"></p>
         </div>
-        <div id="esp_section_2">
-            <p>Sur le lieu même de la future basilique, une première église existait, probablement bâtie vers 1657. Durant la Révolution, elle fut transformée en hôpital. En octobre 1870, les saints patrons de la ville de Besançon furent invoqués par le
-                Cardinal Mathieu pour protéger la ville des troupes prussiennes. La ville ayant été épargnée, celui-ci prononce un vœu solennel le 26 janvier 1871 : il s'agit d'ériger un nouvel édifice sur le lieu que la tradition locale reconnaissait
-                comme la grotte qui avait abrité les deux saints.</p>
-        </div>
-        <div id="esp_section_3">
-            <p>En novembre 1870, un premier projet est déposé par l'architecte franc-comtois Alfred Ducat, à titre gracieux. Mais Edouard Bérard, élève de Viollet-le-Duc, sera désigné pour réaliser l'édifice. Or, ce deuxième projet s'avérant trop coûteux,
-                c'est le projet d'Alfred Ducat qui sera finalement retenu.</p>
-        </div>
-        <div id="esp_section_4">
+        <p id="esp_section_2">Sur le lieu même de la future basilique, une première église existait, probablement bâtie vers 1657. Durant la Révolution, elle fut transformée en hôpital. En octobre 1870, les saints patrons de la ville de Besançon furent invoqués par le Cardinal Mathieu pour protéger la ville des troupes prussiennes. La ville ayant été épargnée, celui-ci prononce un vœu solennel le 26 janvier 1871 : il s'agit d'ériger un nouvel édifice sur le lieu que la tradition locale reconnaissait comme la grotte qui avait abrité les deux saints.</p>
+        <p id="esp_section_3">En novembre 1870, un premier projet est déposé par l'architecte franc-comtois Alfred Ducat, à titre gracieux. Mais Edouard Bérard, élève de Viollet-le-Duc, sera désigné pour réaliser l'édifice. Or, ce deuxième projet s'avérant trop coûteux, c'est le projet d'Alfred Ducat qui sera finalement retenu.</p>
+        <p id="esp_section_4" class="img_container">
             <img src="assets/images/esp_02.jpg" width="738" height="425" alt="Photo de la basilique datant du vingtième siècle en noir et blanc. Il est écrit un résumé de l'historique de la basilique. Elle a été créée par l'architecte M. Ducat à la fin du dix-neuvième siècle dédié aux Saints Ferreol et Ferjeux, décapités en 212 à Besançon où ils étaient venus prêcher le christianisme.">
-        </div>
-        <div id="esp_section_5">
+        </p>
+        <p id="esp_section_5" class="img_container">
             <img src="assets/images/esp_03.jpg" width="328" height="425" alt="Photo de la façade principale de la basilique datant du vingtième siècle en noir et blanc. On peut voir les deux tours qui ont respectivement à gauche un baromètre et à droite une horloge.">
-        </div>
-        <div id="esp_section_6">
-            <p>Celui-ci reste dans l'esprit des constructions de l'époque : Notre-Dame-de-Fourvière à Lyon, le Sacré-Cœur de Montmartre à Paris ou encore la cathédrale de Marseille. Les premiers travaux commencent cette même année, et une première messe
-                est célébrée en 1892 dans la partie déjà construite. La crypte est inaugurée en 1895. En 1898, Alfred Ducat décède, et son disciple et collaborateur, Joseph Simonin, achève la construction.</p>
-        </div>
-        <div id="esp_section_7">
-            <p>En 1905, la ville de Besançon en obtient la propriété et la charge. En 1912, l'église est érigée officiellement en basilique. L'ombrellino et le tintinnabulum qui se trouvent de part et d'autre du transept sont les emblèmes réservés aux basiliques.
-                Le 21 juin 1925, elle est enfin consacrée, soit cinquante-quatre années après le vœu du Cardinal Mathieu.</p>
-        </div>
-        <div id="esp_section_8">
+        </p>
+        <p id="esp_section_6">Celui-ci reste dans l'esprit des constructions de l'époque : Notre-Dame-de-Fourvière à Lyon, le Sacré-Cœur de Montmartre à Paris ou encore la cathédrale de Marseille. Les premiers travaux commencent cette même année, et une première messe est célébrée en 1892 dans la partie déjà construite. 
+        La crypte est inaugurée en 1895. En 1898, Alfred Ducat décède, et son disciple et collaborateur, Joseph Simonin, achève la construction.</p>
+        <p id="esp_section_7">En 1905, la ville de Besançon en obtient la propriété et la charge. En 1912, l'église est érigée officiellement en basilique. L'ombrellino et le tintinnabulum qui se trouvent de part et d'autre du transept sont les emblèmes réservés aux basiliques.
+        Le 21 juin 1925, elle est enfin consacrée, soit cinquante-quatre années après le vœu du Cardinal Mathieu.</p>
+        <p id="esp_section_8" class="img_container">
             <picture>
                 <source srcset="assets/images/esp_04_mobile.jpg" media="(max-width: 765px)">
                 <img class="panorama" src="assets/images/esp_04.jpg" width="2048" height="873" alt="Photo de la basilique moderne vue de loin. On distingue l'intégralité du monument mais aussi le grand cimetière de Saint-Ferjeux situé juste devant.">
             </picture>
-        </div>
+        </p>
     </main>
     <footer>
         <?php include("footer.html")?>
