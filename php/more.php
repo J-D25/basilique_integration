eaf65c0d9dec5e42b96dba7dccb99cef449bc235
J-D25 <jd.auzanneau@icloud.com>
2022-07-06 13:15:52 +0200

    php/more.php : optimize

--- php/more.php
+++ php/more.php
@@ -1,16 +1,19 @@
 <?php
-include('head.php');
-$_POST = json_decode(file_get_contents('php://input'), true);
+include('head.php'); //Appel des variables globales 
+include('conn.php'); //Démarrage connexion
+$input = json_decode(file_get_contents('php://input'), true);
 $limit = 5;
-$offset=$limit*$_POST;
+$offset=$limit*$input;
 $errorCode = true;
 try{
-    $conn = new PDO("mysql:host=".SERVER.";dbname=".DATABASE."", USERNAME, PASSWORD);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-    $sth = $conn->prepare("SELECT `email`, `date` FROM `newsletter` ORDER BY `email` LIMIT ".$limit." OFFSET ".$offset);
+    $sth = $conn->prepare("SELECT `email`, `date` FROM `newsletter` ORDER BY `email` LIMIT :limit OFFSET :offset");
+    $sth->bindParam(':limit', $limit, PDO::PARAM_INT);
+    $sth->bindParam(':offset', $offset, PDO::PARAM_INT);
     $sth->execute();
     $res=$sth->fetchAll(PDO::FETCH_ASSOC);
+    $sth2 = $conn->prepare("SELECT COUNT(`email`) as `totalNumberEmail` FROM `newsletter`");
+    $sth2->execute();
+    $res2=$sth2->fetchAll(PDO::FETCH_ASSOC);
 }
 catch(PDOException $e){
     $errorCode = $e->getCode();
@@ -18,18 +21,7 @@ catch(PDOException $e){
 $conn = null;
 echo json_encode(["data"=>$res]);
 
-try{
-    $conn = new PDO("mysql:host=".SERVER.";dbname=".DATABASE."", USERNAME, PASSWORD);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-    $sth = $conn->prepare("SELECT COUNT(`email`) as `totalNumberEmail` FROM `newsletter`");
-    $sth->execute();
-    $res=$sth->fetchAll(PDO::FETCH_ASSOC);
-}
-catch(PDOException $e){
-    $errorCode = $e->getCode();
-}
-$record=json_encode($res[0]);
+$record=json_encode($res2[0]);
 header('Record-number: '.$record);
 header('Select-number: '.($offset+$limit));
 ?>
