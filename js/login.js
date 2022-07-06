a4eb9cb04f0352e97ca6aff335347263641ea0f4
J-D25 <jd.auzanneau@icloud.com>
2022-07-06 14:02:09 +0200

    js/login.js : add comment + optimize

--- js/login.js
+++ js/login.js
@@ -1,14 +1,14 @@
+//Formulaire de connexion
 const formLogin = document.querySelector("#login_form");
 const inputLogin = document.querySelectorAll('.login_input');
-const btnSubmitLogin = document.querySelector('#login_button');
 
 formLogin.addEventListener('submit', function(e) {
     e.preventDefault();
-    checkLogin();
     ajaxLogin();
 });
 
 function checkLogin() {
+    let errorCount = 0; //initialisation du compteur d'erreurs
     inputLogin.forEach((input) => {
         const input_span = document.createElement("SPAN");
         const cannot = "Ce champ ne peut être vide";
@@ -18,6 +18,7 @@ function checkLogin() {
         input_span.setAttribute("class", "login_form_span_invalid");
         input_span.setAttribute("id", input.id + "_invalid");
         if (input.validity.valueMissing) {
+            errorCount++;
             input.classList.add("login_form_invalid");
             if (!input_spanInvalid) {
                 input.insertAdjacentElement('afterend', input_span);
@@ -28,24 +29,31 @@ function checkLogin() {
             input.classList.remove("login_form_invalid");
             if (input_spanInvalid) {
                 input_spanInvalid.remove();
-            }
+            };
         }
-    })
+    });
+
+    if (errorCount === 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 function ajaxLogin() {
-    const formData = new FormData(formLogin);
-    fetch("php/log.php", {
-            method: "POST",
-            body: formData
-        })
-        .then(res => res.text())
-        .then((results) => {
-            let resultsJSON = JSON.parse(results);
-            if (resultsJSON.responseServer === true && resultsJSON.responseDB === true && resultsJSON.connection === true) {
-                document.location.href = "admin.php";
-            } else if (resultsJSON.responseServer === true && resultsJSON.responseDB === true && resultsJSON.connection === false) {
-                alert("Identifiant ou mot de passe incorrect.");
-            }
-        })
+    if (checkLogin()) { //Lancement de checkLogin() + lancement requête si pas d'erreur.
+        const formData = new FormData(formLogin);
+        fetch("php/log.php", {
+                method: "POST",
+                body: formData
+            })
+            .then(response => response.json())
+            .then((result) => {
+                if (result.responseServer === true && result.responseDB === true && result.connection === true) {
+                    document.location.href = "admin.php";
+                } else if (result.responseServer === true && result.responseDB === true && result.connection === false) {
+                    alert("Identifiant ou mot de passe incorrect.");
+                }
+            });
+    }
 }
