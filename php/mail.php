83419fd9c3092653bd7dc179dc49b72319b065bc
J-D25 <jd.auzanneau@icloud.com>
2022-07-06 14:56:39 +0200

    php/mail.php : change variable name, add responseMailer

--- php/mail.php
+++ php/mail.php
@@ -1,22 +1,35 @@
 <?php
-    if (isset($_POST['nom']) && isset($_POST['prénom']) && isset($_POST['mail']) && isset($_POST['objet']) && isset($_POST['message'])) {
-        if(!empty($_POST['nom']) && !empty($_POST['prénom']) && !empty($_POST['mail']) && filter_var($_POST['mail'], FILTER_VALIDATE_EMAIL) && !empty($_POST['objet']) && !empty($_POST['message'])) {
-            $expéditeur=$_POST['nom'] . ' ' . $_POST['prénom'] . ' <' . $_POST['mail'].'>';
-            $entete  = 'MIME-Version: 1.0' . "\r\n";
-            $entete .= 'Content-type: text/html; charset=utf-8' . "\r\n";
-            $entete .= 'From: Basilique Saint-Ferjeux <basilique@promo-159.codeur.online>' . "\r\n";
-            $entete .= 'Reply-to: ' . $expéditeur ;
-            $objet = 'Contact - Basilique Saint-Ferjeux';
+    if (isset($_POST['lname']) && isset($_POST['fname']) && isset($_POST['mail']) && isset($_POST['object']) && isset($_POST['message'])) {
+        if(!empty($_POST['lname']) && !empty($_POST['fname']) && !empty($_POST['mail']) && filter_var($_POST['mail'], FILTER_VALIDATE_EMAIL) && !empty($_POST['object']) && !empty($_POST['message'])) {
+            //Variables
+            $firstName = htmlspecialchars($_POST['fname']);
+            $lastName = htmlspecialchars($_POST['lname']);
+            $objectContent = htmlspecialchars($_POST['object']);
+            $mailContent = htmlspecialchars($_POST['mail']);
+            $messageContent = htmlspecialchars($_POST['message']);
+            
+            //Contenu mail
+            $sender=$lastName . ' ' . $firstName . ' <' . $mailContent.'>';
+            $header  = 'MIME-Version: 1.0' . "\r\n";
+            $header .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+            $header .= 'From: Basilique Saint-Ferjeux <basilique@promo-159.codeur.online>' . "\r\n";
+            $header .= 'Reply-to: ' . $sender ;
+            $object = 'Contact - Basilique Saint-Ferjeux';
             $message = '<p>Message envoyé depuis la page Contact de la Basilique Saint-Ferjeux</p>
-            <p><b>Nom et prénom : </b>' . $_POST['nom'] . ' ' . $_POST['prénom'] . '<br>
-            <p><b>Email : </b>' . $_POST['mail'] . '<br>
-            <p><b>Objet : </b>' . $_POST['objet'] . '<br>
-            <b>Message : </b>' . htmlspecialchars($_POST['message']) . '</p>';
-            $destinataire = 'Basilique Saint-Ferjeux <j.auzanneau@codeur.online>, ' . $expéditeur;
-            $retour = mail($destinataire, $objet, $message, $entete);
-            echo json_encode(["response"=>true]);
+            <p><b>Nom et prénom : </b>' . $lastName . ' ' . $firstName . '</p>
+            <p><b>Email : </b>' . $mailContent . '</p>
+            <p><b>Objet : </b>' . $objectContent . '</p>
+            <b>Message : </b>' . $messageContent . '</p>';
+            $receiver = 'Basilique Saint-Ferjeux <j.auzanneau@codeur.online>, ' . $sender;
+            $response = mail($receiver, $object, $message, $header);
+            if ($response===true){
+                echo json_encode(["responseServer"=>true, "responseMailer"=>true]);
+            }else{
+                echo json_encode(["responseServer"=>true, "responseMailer"=>false]);
+            }
         } else {
             echo json_encode(["response"=>false]);
         }
     }
 ?>
+
