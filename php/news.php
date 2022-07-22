630880a4f9dda45057b8c324197ae59a20b86809
J-D25 <jd.auzanneau@icloud.com>
2022-07-22 14:39:07 +0200

    php/news.php : add filter_input

--- php/news.php
+++ php/news.php
@@ -2,13 +2,13 @@
 include('head.php'); //Appel des variables globales
 
 if (isset($_POST['newsletter_email']) && !empty($_POST['newsletter_email']) && filter_var($_POST['newsletter_email'], FILTER_VALIDATE_EMAIL)) {
-
+    $email = filter_input(INPUT_GET, $_POST['newsletter_email'], FILTER_SANITIZE_EMAIL);
     $errorCode = true;
     try{
         $conn = new PDO("mysql:host=".SERVER.";dbname=".DATABASE."", USERNAME, PASSWORD);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $sql = $conn->prepare("INSERT INTO newsletter (email) VALUES ( :email )");
-        $sql->bindParam(':email', $_POST['newsletter_email'], PDO::PARAM_STR);
+        $sql->bindParam(':email', $email, PDO::PARAM_STR);
         $sql->execute();
     }
     catch(PDOException $e){
