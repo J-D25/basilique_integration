c9292282c0dec8713df01fee696c3cffa28c187a
J-D25 <jd.auzanneau@icloud.com>
2022-07-22 14:40:36 +0200

    php/log.php : add htmlspecialchars

--- php/log.php
+++ php/log.php
@@ -2,11 +2,12 @@
 include('head.php'); //Appel des variables globales
 if (isset($_POST['username']) && !empty($_POST['username']) && isset($_POST['password']) && !empty($_POST['password'])) {
     $errorCode = true;
+    $user = htmlspecialchars($_POST['username']);
     try{
         $conn = new PDO("mysql:host=".SERVER.";dbname=".DATABASE."", USERNAME, PASSWORD);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $sth = $conn->prepare("SELECT `username`, `password` FROM `users` WHERE `username`=:username");
-        $sth->bindValue(':username', $_POST['username'], PDO::PARAM_STR);
+        $sth->bindParam(':username', $user, PDO::PARAM_STR);
         $sth->execute();
         $res = $sth->fetch(PDO::FETCH_ASSOC);
     }
@@ -17,7 +18,7 @@ if (isset($_POST['username']) && !empty($_POST['username']) && isset($_POST['pas
     if (isset($res['password']) && password_verify($_POST['password'], $res['password'])){
         echo json_encode(["responseServer"=>true, "responseDB"=>$errorCode, "connection"=>true]);
         session_start();
-        $_SESSION['user'] = $_POST['username'];
+        $_SESSION['user'] = $user;
     } else {
         echo json_encode(["responseServer"=>true, "responseDB"=>$errorCode, "connection"=>false]);
     }
